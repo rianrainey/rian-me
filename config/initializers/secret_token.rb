f163986b92b48441bd3c4f3066b0715604b5a1e0
Rian Rainey <rian@rianrainey.com>
2013-04-21 08:48:16 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RianMe::Application.config.secret_token = 'ec35f1e7ba0712941a1ed4396da702e9034ea7f4680c16105b68719cf09ea4cf943995aef71eef2c1756f1cd2d411d7dcd48e158859841f501ae8e4aec04c618'
